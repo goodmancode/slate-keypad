b9c2e0a7e33510d4482bd557daccc1e967a01e01
goodmancode <jacob.j.goodman@gmail.com>
2021-06-21 04:26:04 -0400

    Initial website files

---
+++ website/index.php
@@ -0,0 +1,51 @@
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+    
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    
+    <!-- Page Title -->
+    <title>Slate</title>
+  </head>
+  
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="">Slate Macro Keypad</a>
+        <a class="btn btn-outline-primary" href="https://github.com/goodmancode/slate-keypad" role="button"><i class="bi-github me-2" role="img" aria-label="GitHub"></i>Github</a>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row my-5">
+        <h1 class="display-1 text-center">Slate</h1>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <img src="img/keypad_render_transparent.png" alt="keypad render"> 
+        </div>
+      </div>
+      <div class="row my-5">
+        <h1 class="display-6 text-center">A multi-input programmable wireless macro keypad.</h1>
+      </div>
+      <div class="row">
+        <div class="card border-danger">
+          <div class="card-body text-center text-danger">
+            <strong>Under Development</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+  </body>
+</html>
