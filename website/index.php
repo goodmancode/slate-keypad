e0a4fb991dfb3cf1d07f13457e87f4f6311b3b0d
goodmancode <jacob.j.goodman@gmail.com>
2021-06-21 13:06:09 -0400

    Add logo and adjust navbar

--- website/index.php
+++ website/index.php
@@ -6,7 +6,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
-    
+    <!--<link rel="stylesheet" href="css/bootstrap_darkly.css">-->
+
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
     
@@ -15,28 +16,47 @@
   </head>
   
   <body>
-    <nav class="navbar navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="">Slate Macro Keypad</a>
-        <a class="btn btn-outline-primary" href="https://github.com/goodmancode/slate-keypad" role="button"><i class="bi-github me-2" role="img" aria-label="GitHub"></i>Github</a>
-      </div>
-    </nav>
+  <nav class="navbar navbar-light navbar-expand-md bg-light justify-content-center">
     <div class="container">
-      <div class="row my-5">
-        <h1 class="display-1 text-center">Slate</h1>
-      </div>
-      <div class="row justify-content-center">
+        <a href="/" class="navbar-brand d-flex me-auto">
+          <img src="img/slate_logo_light.svg" alt="" width="48" height="48">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsingNavbar3">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
+            <ul class="nav navbar-nav ms-auto w-100 justify-content-end">
+                <li class="nav-item px-1 h-100">
+                    <a class="nav-link" href="https://github.com/goodmancode/slate-keypad"><i class="bi bi-github me-2"></i>Github</a>
+                </li>
+                <li class="nav-item px-1 h-100">
+                    <a class="nav-link disabled" href="#"><i class="bi bi-files me-2"></i>Docs</a>
+                </li>
+                <li class="nav-item px-1 h-100">
+                    <a class="nav-link disabled" href="#"><i class="bi bi-people-fill me-2"></i>About</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  </nav>
+    <div class="container">
+      <div class="row mt-5 justify-content-center">
         <div class="col-4">
-          <img src="img/keypad_render_transparent.png" alt="keypad render"> 
+          <img class="img-fluid" src="img/keypad_render_transparent.png" alt="keypad render"> 
         </div>
       </div>
-      <div class="row my-5">
-        <h1 class="display-6 text-center">A multi-input programmable wireless macro keypad.</h1>
+      <div class="row mt-5">
+        <h1 class="display-1 text-center">Slate</h1>
       </div>
       <div class="row">
-        <div class="card border-danger">
-          <div class="card-body text-center text-danger">
-            <strong>Under Development</strong>
+        <h1 class="display-6 text-center">A multi-input programmable wireless macro keypad.</h1>
+      </div>
+      <div class="row mt-5 justify-content-center">
+        <div class="col-8">
+          <div class="card border-danger">
+            <div class="card-body text-center text-danger">
+              <strong>Under Development</strong>
+            </div>
           </div>
         </div>
       </div>
