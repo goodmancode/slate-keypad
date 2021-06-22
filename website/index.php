ca9a86c7784ed602aaf1a4cc9afce00d783494b7
goodmancode <jacob.j.goodman@gmail.com>
2021-06-22 10:10:17 -0400

    Modularize website components

--- website/index.php
+++ website/index.php
@@ -1,44 +1,10 @@
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
-    <!--<link rel="stylesheet" href="css/bootstrap_darkly.css">-->
-
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
-    
-    <!-- Page Title -->
-    <title>Slate</title>
-  </head>
-  
+  <!-- Read in html head -->
+  <?php readfile('./html/head.html'); ?>
   <body>
-  <nav class="navbar navbar-light navbar-expand-md bg-light justify-content-center">
-    <div class="container">
-        <a href="/" class="navbar-brand d-flex me-auto">
-          <img src="img/slate_logo_light.svg" alt="" width="48" height="48">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsingNavbar3">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
-            <ul class="nav navbar-nav ms-auto w-100 justify-content-end">
-                <li class="nav-item px-1 h-100">
-                    <a class="nav-link" href="https://github.com/goodmancode/slate-keypad"><i class="bi bi-github me-2"></i>Github</a>
-                </li>
-                <li class="nav-item px-1 h-100">
-                    <a class="nav-link disabled" href="#"><i class="bi bi-files me-2"></i>Docs</a>
-                </li>
-                <li class="nav-item px-1 h-100">
-                    <a class="nav-link disabled" href="#"><i class="bi bi-people-fill me-2"></i>About</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-  </nav>
+    <!-- Read in navbar element -->
+    <?php readfile("./html/navbar.html"); ?>
     <div class="container">
       <div class="row mt-5 justify-content-center">
         <div class="col-4">
@@ -61,11 +27,7 @@
         </div>
       </div>
     </div>
-
-
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <!-- Read in required scripts -->
+    <?php readfile('./html/scripts.html'); ?>
   </body>
 </html>
