78681fcca7f63f41c255918cd777b317bdd00c38
goodmancode <jacob.j.goodman@gmail.com>
2021-09-25 22:20:47 -0400

    Use simplemath module for mapping to range

--- slate-software/helpers.py
+++ slate-software/helpers.py
@@ -1,9 +1,6 @@
 # A collection of helper functions
-
-# Simulates arduino map() function
-def scale(value, fromLow, fromHigh, toLow, toHigh):
-    return (((value - fromLow) * (toHigh - toLow)) / (fromHigh - fromLow)) + toLow
+from adafruit_simplemath import map_range
 
 # Scales analog joystick values in range -100 to 100 (adjust to desired min/max)
 def scaleJoyPosition(position):
-    return (round(scale(position[0], 300, 65000, -100, 100)), round(scale(position[1], 300, 65000, -100, 100)))
+    return (round(map_range(position[0], 300, 65000, -100, 100)), round(map_range(position[1], 300, 65000, -100, 100)))
