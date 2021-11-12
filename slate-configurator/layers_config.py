33d303be4a13570326ca265ba3bc4ae41c8c21c1
goodmancode <jacob.j.goodman@gmail.com>
2021-11-12 18:32:50 -0500

    layers_config now same as layers.py

--- slate-configurator/layers_config.py
+++ slate-configurator/layers_config.py
@@ -1,3 +1,6 @@
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+
 MEDIA = 1
 KEY = 2
 STRING = 3
@@ -6,6 +9,10 @@ KEY_RELEASE = 5
 CHANGE_LAYER = 6
 MOUSE_CLICK = 7
 MOUSE_MOVE = 8
+
+LEFT_BUTTON = 1
+MIDDLE_BUTTON = 4
+RIGHT_BUTTON = 2
 
 slate_config = {
     "layers": [
@@ -121,178 +128,67 @@ slate_config = {
             }
         },
         {
-            "name": "Slate2",
+            "name": "Youtube Controls",
             "touch_shortcuts": [
                 {
-                    "label": "Yeet",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Play",
+                    "icon": "icons/pr_play.bmp",
+                    "actions": (KEY, [Keycode.K]),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Pause",
+                    "icon": "icons/pr_pause.bmp",
+                    "actions": (KEY, [Keycode.K]),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Rewind",
+                    "icon": "icons/pr_rewind.bmp",
+                    "actions": (KEY, [Keycode.LEFT_ARROW]),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "FastForward",
+                    "icon": "icons/pr_ffwd.bmp",
+                    "actions": (KEY, [Keycode.RIGHT_ARROW]),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Previous",
+                    "icon": "icons/pr_previous.bmp",
+                    "actions": (KEY, [Keycode.RIGHT_SHIFT, Keycode.P]),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Next",
+                    "icon": "icons/pr_next.bmp",
+                    "actions": (KEY, [Keycode.RIGHT_SHIFT, Keycode.N]),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Vol -",
+                    "icon": "icons/pr_voldown.bmp",
+                    "actions": (MEDIA, ConsumerControlCode.VOLUME_DECREMENT),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Vol +",
+                    "icon": "icons/pr_volup.bmp",
+                    "actions": (MEDIA, ConsumerControlCode.VOLUME_INCREMENT),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Fullscreen",
+                    "icon": "icons/pr_fullscreen.bmp",
+                    "actions": (KEY, [Keycode.F]),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Slow",
+                    "icon": "icons/pr_slow.bmp",
+                    "actions": (KEY, [Keycode.RIGHT_SHIFT, Keycode.COMMA]),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Fast",
+                    "icon": "icons/pr_fast.bmp",
+                    "actions": (KEY, [Keycode.RIGHT_SHIFT, Keycode.PERIOD]),
                 },
                 {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-            ],
-            "key_shortcuts": [
-                {
-                    "assigned_key": 0,
-                    "actions": (KEY, 0x04),
-                },
-                {
-                    "assigned_key": 1,
-                    "actions": (STRING, "Keypad"),
-                },
-                {
-                    "assigned_key": 2,
-                    "actions": (None, None),
-                },
-                {
-                    "assigned_key": 3,
-                    "actions": (None, None),
-                },
-                {
-                    "assigned_key": 4,
-                    "actions": (None, None),
-                },
-                {
-                    "assigned_key": 5,
-                    "actions": (None, None),
-                },
-                {
-                    "assigned_key": 6,
-                    "actions": (None, None),
-                },
-                {
-                    "assigned_key": 7,
-                    "actions": (None, None),
-                },
-            ],
-            "encoder": {
-                "increment": (MEDIA, 0xE9),
-                "decrement": (MEDIA, 0xEA),
-                "button": (MEDIA, 0xCD),
-            },
-            "joystick": {
-                "x+": (MOUSE_MOVE, [-100, 0, 0]),
-                "x-": (MOUSE_MOVE, [100, 0, 0]),
-                "y+": (MOUSE_MOVE, [0, 100, 0]),
-                "y-": (MOUSE_MOVE, [0, -100, 0]),
-                "button": (MOUSE_CLICK, 1),
-            }
-        },
-        {
-            "name": "Slate3",
-            "touch_shortcuts": [
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
-                },
-                {
-                    "label": "Slate Logo",
-                    "icon": "icons/slate.bmp",
-                    "actions": (STRING, "Slate"),
+                    "label": "Mute",
+                    "icon": "icons/pr_mute.bmp",
+                    "actions": (KEY, [Keycode.M]),
                 },
             ],
             "key_shortcuts": [
